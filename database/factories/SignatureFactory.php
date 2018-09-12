29c440712bd6b674f20e3d142d80d554444d753d
Jesray Garciano <jesray.garciano@gmail.com>
2018-09-12 11:03:48 +0800

    Modified Factory File

--- database/factories/SignatureFactory.php
+++ database/factories/SignatureFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(App\Signature::class, function (Faker $faker) {
     return [
         //
         'name' => $faker->name,
